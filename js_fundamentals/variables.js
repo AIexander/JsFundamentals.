b7fce729131ba7b3642bccff1f29e6ef5acf977f
Christian <chrizx0@gmail.com>
2016-06-29 17:24:32 -0400

    reverse temp formula

--- js_fundamentals/variables.js
+++ js_fundamentals/variables.js
@@ -61,8 +61,14 @@ console.log ("The area is "+area )
 var tempInC = 50
 var tempInF = (tempInC*1.8) + 32
 
+
+
 console.log (tempInC+"°C is " + tempInF + "°F")
 // Store a celsius temperature into a variable.
 // Convert it to fahrenheit and output "NN°C is NN°F".
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C."
+var tempInF = 50
+var tempInC = (tempInF-32)*.5556
+
+console.log (tempInF+"°F is " + tempInC + "°C")
