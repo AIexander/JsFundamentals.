51fb81ade09689c0848da6b60d716e6559105eef
Christian <chrizx0@gmail.com>
2016-06-30 17:43:23 -0400

    arrays

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -6,5 +6,5 @@
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
-
-
+var colors = ["green", "blue", "black", "penguin", "orange"];
+console.log("My #1 choice is "+ colors[0])
